5f9334b7e5c07b8f6c20f8f6a230ed5cb40caa77
Juan <jnvleite@gmail.com>
2019-10-08 15:09:47 -0300

    auth routes

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,21 @@
 import {createAppContainer, createSwitchNavigator} from 'react-navigation';
-// import {createBottomTabNavigator} from 'react-navigation-tabs';
+import {createBottomTabNavigator} from 'react-navigation-tabs';
 
 import SignIn from '~/screens/SignIn';
 import SignUp from '~/screens/SignUp';
+import Dashboard from '~/screens/Dashboard';
 
-const Routes = createAppContainer(createSwitchNavigator({SignIn, SignUp}));
+const Routes = (isSigned = false) =>
+  createAppContainer(
+    createSwitchNavigator(
+      {
+        Sign: createSwitchNavigator({SignIn, SignUp}),
+        App: createBottomTabNavigator({Dashboard}),
+      },
+      {
+        initialRouteName: isSigned ? 'App' : 'Sign',
+      },
+    ),
+  );
 
 export default Routes;
