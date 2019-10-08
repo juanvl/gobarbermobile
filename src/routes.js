6d5df2882d2fd572a8b335fb2bddcbb06fa1beb3
Juan <jnvleite@gmail.com>
2019-10-08 15:31:56 -0300

    tab navigation

--- src/routes.js
+++ src/routes.js
@@ -4,13 +4,26 @@ import {createBottomTabNavigator} from 'react-navigation-tabs';
 import SignIn from '~/screens/SignIn';
 import SignUp from '~/screens/SignUp';
 import Dashboard from '~/screens/Dashboard';
+import Profile from '~/screens/Profile';
 
 const Routes = (isSigned = false) =>
   createAppContainer(
     createSwitchNavigator(
       {
         Sign: createSwitchNavigator({SignIn, SignUp}),
-        App: createBottomTabNavigator({Dashboard}),
+        App: createBottomTabNavigator(
+          {Dashboard, Profile},
+          {
+            tabBarOptions: {
+              keyboardHidesTabBar: true,
+              activeTintColor: '#fff',
+              inactiveTintColor: 'rgba(255,255,255, 0.6)',
+              style: {
+                backgroundColor: '#8d41a8',
+              },
+            },
+          },
+        ),
       },
       {
         initialRouteName: isSigned ? 'App' : 'Sign',
