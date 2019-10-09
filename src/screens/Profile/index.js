c3ba6dc97b820e87d28e137b8bbdd6d8f4ec2e2b
Juan <jnvleite@gmail.com>
2019-10-09 15:03:53 -0300

    profile

--- src/screens/Profile/index.js
+++ src/screens/Profile/index.js
@@ -1,12 +1,115 @@
-import React from 'react';
+import React, {useState, useRef} from 'react';
+import {useDispatch, useSelector} from 'react-redux';
 import Icon from 'react-native-vector-icons/MaterialIcons';
 
+import {updateProfileRequest} from '~/store/modules/user/actions';
 import Background from '~/components/Background';
 
-// import { Container } from './styles';
+import * as S from './styles';
 
 export default function Profile() {
-  return <Background />;
+  const profile = useSelector(state => state.user.profile);
+
+  const [name, setName] = useState(profile.name);
+  const [email, setEmail] = useState(profile.email);
+  const [oldPassword, setOldPassword] = useState('');
+  const [password, setPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
+
+  const emailRef = useRef();
+  const pwdRef = useRef();
+  const oldPwdRef = useRef();
+  const confirmPwdRef = useRef();
+
+  const dispatch = useDispatch();
+
+  function handleSubmit() {
+    dispatch(
+      updateProfileRequest({
+        name,
+        email,
+        oldPassword,
+        password,
+        confirmPassword,
+      }),
+    );
+
+    setOldPassword('');
+    setPassword('');
+    setConfirmPassword('');
+  }
+
+  return (
+    <Background>
+      <S.Container>
+        <S.Title>Meu perfil</S.Title>
+
+        <S.Form>
+          <S.FormInput
+            icon="person-outline"
+            autoCorrect={false}
+            placeholder="Nome completo"
+            returnKeyType="next"
+            onSubmitEditing={() => emailRef.current.focus()}
+            value={name}
+            onChangeText={setName}
+          />
+
+          <S.FormInput
+            icon="mail-outline"
+            keyboardType="email-address"
+            autoCapitalize="none"
+            autoCorrect={false}
+            placeholder="Seu email"
+            ref={emailRef}
+            returnKeyType="next"
+            onSubmitEditing={() => oldPwdRef.current.focus()}
+            value={email}
+            onChangeText={setEmail}
+          />
+
+          <S.Separator></S.Separator>
+
+          <S.FormInput
+            icon="lock-outline"
+            secureTextEntry
+            placeholder="Sua senha atual"
+            ref={oldPwdRef}
+            returnKeyType="next"
+            onSubmitEditing={() => pwdRef.current.focus()}
+            value={oldPassword}
+            onChangeText={setOldPassword}
+          />
+
+          <S.FormInput
+            icon="lock-outline"
+            secureTextEntry
+            placeholder="Sua senha secreta"
+            ref={pwdRef}
+            returnKeyType="next"
+            onSubmitEditing={() => confirmPwdRef.current.focus()}
+            value={password}
+            onChangeText={setPassword}
+          />
+
+          <S.FormInput
+            icon="lock-outline"
+            secureTextEntry
+            placeholder="Confirme sua nova senha"
+            ref={confirmPwdRef}
+            returnKeyType="send"
+            onSubmitEditing={handleSubmit}
+            value={confirmPassword}
+            onChangeText={setConfirmPassword}
+          />
+
+          <S.SubmitButton onPress={handleSubmit}>
+            Atualizar perfil
+          </S.SubmitButton>
+        </S.Form>
+      </S.Container>
+    </Background>
+  );
 }
 
 Profile.navigationOptions = {
--- src/screens/Profile/styles.js
+++ src/screens/Profile/styles.js
@@ -1,3 +1,36 @@
 import styled from 'styled-components/native';
+import Input from '~/components/Input';
+import Button from '~/components/Button';
 
-export const Container = styled.View``;
+export const Container = styled.SafeAreaView`
+  flex: 1;
+`;
+
+export const Title = styled.Text`
+  color: #fff;
+  margin-top: 30px;
+  font-size: 20px;
+  font-weight: bold;
+  align-self: center;
+`;
+
+export const Form = styled.ScrollView.attrs({
+  showsVerticalScrollIndicator: false,
+  contentContainerStyle: {padding: 30},
+})`
+  align-self: stretch;
+`;
+
+export const FormInput = styled(Input)`
+  margin-bottom: 10px;
+`;
+
+export const SubmitButton = styled(Button)`
+  margin-top: 5px;
+`;
+
+export const Separator = styled.View`
+  height: 1px;
+  background-color: rgba(255, 255, 255, 0.2);
+  margin: 20px 0 30px;
+`;
