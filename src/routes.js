29994da45b9a315a4371d28b3b942131120a4954
Juan <jnvleite@gmail.com>
2019-10-09 16:23:06 -0300

    navigator agendar

--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,17 @@
+import React from 'react';
 import {createAppContainer, createSwitchNavigator} from 'react-navigation';
 import {createBottomTabNavigator} from 'react-navigation-tabs';
+import {createStackNavigator} from 'react-navigation-stack';
+
+import Icon from 'react-native-vector-icons/MaterialIcons';
 
 import SignIn from '~/screens/SignIn';
 import SignUp from '~/screens/SignUp';
 import Dashboard from '~/screens/Dashboard';
 import Profile from '~/screens/Profile';
+import SelectDateTime from '~/screens/New/SelectDateTime';
+import SelectProvider from '~/screens/New/SelectProvider';
+import Confirm from '~/screens/New/Confirm';
 
 const Routes = (isSigned = false) =>
   createAppContainer(
@@ -12,7 +19,39 @@ const Routes = (isSigned = false) =>
       {
         Sign: createSwitchNavigator({SignIn, SignUp}),
         App: createBottomTabNavigator(
-          {Dashboard, Profile},
+          {
+            Dashboard,
+            New: {
+              screen: createStackNavigator(
+                {
+                  SelectProvider,
+                  SelectDateTime,
+                  Confirm,
+                },
+                {
+                  defaultNavigationOptions: {
+                    headerTransparent: true,
+                    headerTintColor: '#fff',
+                    headerLeftContainerStyle: {
+                      marginLeft: 20,
+                    },
+                  },
+                },
+              ),
+              navigationOptions: {
+                tabBarVisible: false,
+                tabBarLabel: 'Agendar',
+                tabBarIcon: (
+                  <Icon
+                    name="add-circle-outline"
+                    size={20}
+                    color="rgba(255,255,255, 0.6)"
+                  />
+                ),
+              },
+            },
+            Profile,
+          },
           {
             tabBarOptions: {
               keyboardHidesTabBar: true,
