3e4a1d3a8c1591ebddbf0cf3bff5c8758cf784b8
Juan <jnvleite@gmail.com>
2019-10-10 10:20:24 -0300

    provider select screen

--- src/screens/New/SelectProvider/index.js
+++ src/screens/New/SelectProvider/index.js
@@ -1,14 +1,45 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import {TouchableOpacity} from 'react-native';
 
 import Icon from 'react-native-vector-icons/MaterialIcons';
 
+import api from '~/services/api';
 import Background from '~/components/Background';
 
-// import { Container } from './styles';
+import * as S from './styles';
 
-export default function SelectProvider() {
-  return <Background />;
+export default function SelectProvider({navigation}) {
+  const [providers, setProviders] = useState([]);
+
+  useEffect(() => {
+    (async () => {
+      const res = await api.get('providers');
+      setProviders(res.data);
+    })();
+  }, []);
+
+  return (
+    <Background>
+      <S.Container>
+        <S.ProvidersList
+          data={providers}
+          renderItem={({item: provider}) => (
+            <S.Provider
+              onPress={() => navigation.navigate('SelectDateTime', {provider})}>
+              <S.Avatar
+                source={{
+                  uri: provider.avatar
+                    ? provider.avatar.url
+                    : `https://api.adorable.io/avatars/50/${provider.name}.png`,
+                }}></S.Avatar>
+              <S.Name>{provider.name}</S.Name>
+            </S.Provider>
+          )}
+          keyExtractor={provider => String(provider.id)}
+        />
+      </S.Container>
+    </Background>
+  );
 }
 
 SelectProvider.navigationOptions = ({navigation}) => ({
